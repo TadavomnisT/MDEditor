bc604096424c1a4c3eddf9d98486773cdd4e0b12
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2023-08-09 22:03:27 +0330

    run a test

--- test.php
+++ test.php
@@ -1,13 +1,65 @@
 <?php
 
 require_once "MDEditor.php";
-
 $mde = new MDEditor;
 
+$mde->setDocumentStyle("light");
 $html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
-var_dump( $html );
+$mde->setDocumentStyle("dark_black");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
-// file_put_contents("./samples/2-which-search-engine-En.html", $html);
+$mde->setDocumentStyle("dark_gray");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodeblack_white");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodegray_white");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodeblack_dark");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodegray_dark");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentWidth(960);
+
+$mde->setDocumentStyle("light");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("dark_black");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("dark_gray");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodeblack_white");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodegray_white");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodeblack_dark");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
+$mde->setDocumentStyle("toggle_darkmodegray_dark");
+$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
+
 
 ?> 
