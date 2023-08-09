965abecd62563ee9ce5cd5947cb729904896615b
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2023-08-09 14:46:16 +0330

    add title and fix stuff

--- MDEditor.php
+++ MDEditor.php
@@ -6,10 +6,12 @@ class MDEditor
 {
     private $parser;
     private $document_style;
+    private $document_title;
 
     public function __construct() {
         $this->parser = new Parsedown();
         $this->documnet_style = "toggle_darkmode_white"; //Default value
+        $this->document_title = "Documnet"; //Default value
     }
 
     public function md2html(string $md_file_path)
@@ -20,28 +22,42 @@ class MDEditor
         return $this->parser->text($markdown); //HTML
     }
 
-    //getter for $document_style
+    //Getter for $document_style
     public function getDocumnetStyle()
     {
         return $this->documnet_style;
     }
 
-    //setter for $document_style
+    //Setter for $document_style
     /*
         Valid inputs:
                        "light" 
-                       "dark" 
+                       "dark_black" 
+                       "dark_gray" 
                        "toggle_darkmode_white" 
                        "toggle_darkmode_dark" 
     */
     public function setDocumnetStyle( string $style )
     {
-        if( $style == "light" || $style == "dark" || $style == "toggle_darkmode_white" || $style == "toggle_darkmode_dark"  )
+        if( $style == "light" || $style == "dark_black" || $style == "dark_gray" || $style == "toggle_darkmode_white" || $style == "toggle_darkmode_dark"  )
         $this->documnet_style = $style;
         else{
             throw new Exception("Inappropriate documnet style.", 1);
             return false;
         }
+        return true;
+    }
+
+    //Getter for $document_title
+    public function getDocumnetTitle()
+    {
+        return $this->document_title;
+    }
+
+    //Setter for $document_title
+    public function setDocumnetTitle( string $title )
+    {
+        $this->document_title = $title;
         return true;
     }
 }
