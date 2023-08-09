4382cfe2cfebb36048fe1ede93d727dae34b0927
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2023-08-10 01:45:24 +0330

    Fix #3
    Fix #3 
    https://github.com/TadavomnisT/MDEditor/issues/3

--- MDEditor.php
+++ MDEditor.php
@@ -181,10 +181,10 @@ class MDEditor
         $header .= '.dark-mode{background-color:#000;color:#fff;}.dark-mode code{background-color:#212121;color:rgb(0,183,255);}*,*:before,*:after{box-sizing:border-box;}.toggle{cursor:pointer;display:inline-block;}.toggle-switch{display:inline-block;background:#ccc;border-radius:16px;width:58px;height:32px;position:relative;vertical-align:middle;transition:background 0.25s;}.toggle-switch:before,.toggle-switch:after{content:"";}.toggle-switch:before{display:block;background:linear-gradient(to bottom,#fff 0%,#eee 100%);border-radius:50%;box-shadow:0 0 0 1px rgba(0,0,0,0.25);width:24px;height:24px;position:absolute;top:4px;left:4px;transition:left 0.25s;}.toggle:hover .toggle-switch:before{background:linear-gradient(to bottom,#fff 0%,#fff 100%);box-shadow:0 0 0 1px rgba(0,0,0,0.5);}.toggle-checkbox:checked + .toggle-switch{background:#3e96df;}.toggle-checkbox:checked + .toggle-switch:before{left:30px;}.toggle-checkbox{position:absolute;visibility:hidden;}.toggle-label{margin-left:5px;position:relative;top:2px;}';
     
         if( $this->getDocumentStyle() == "dark_gray" )
-        $header .= '.dark-mode{background-color:#333;color:#fff;}.dark-mode code{background-color:#212121;color:rgb(0,183,255);}';
+        $header .= '*,html,body{background-color:#333;color:#fff;}code{background-color:#212121 !important;color:rgb(0,183,255) !important;}';
 
         if( $this->getDocumentStyle() == "dark_black" )
-        $header .= '.dark-mode{background-color:#000;color:#fff;}.dark-mode code{background-color:#212121;color:rgb(0,183,255);}';
+        $header .= '*,html,body{background-color:#000;color:#fff;}code{background-color:#212121 !important;color:rgb(0,183,255) !important;}';
 
         $header .= '</style></head><body><div id="container">';
 
