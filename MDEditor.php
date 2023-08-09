bb85fc3657792c226685a151d6e8bef4ed72fe7c
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2023-08-09 12:20:53 +0330

    begining class

--- MDEditor.php
+++ MDEditor.php
@@ -2,8 +2,16 @@
 
 class MDEditor
 {
+    
     public function __construct(Type $var = null) {
         $this->var = $var;
+    }
+
+    public function md2html(string $md_file_path, string $html_file_path)
+    {
+        if (!file_exists( $md_file_path ))
+            throw new Exception("File does not exist.", 1);
+            
     }
 }
 
