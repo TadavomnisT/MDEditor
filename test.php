b458fe01095a4452e753cfa6b2e267664e461ee8
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2023-09-06 17:54:14 +0330

    Test new sample

--- test.php
+++ test.php
@@ -4,62 +4,61 @@ require_once "MDEditor.php";
 $mde = new MDEditor;
 
 $mde->setDocumentStyle("light");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("dark_black");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("dark_gray");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodeblack_white");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodegray_white");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodeblack_dark");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodegray_dark");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentWidth(960);
 
 $mde->setDocumentStyle("light");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("dark_black");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("dark_gray");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodeblack_white");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodegray_white");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodeblack_dark");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
 
 $mde->setDocumentStyle("toggle_darkmodegray_dark");
-$html = $mde->md2html( "./samples/2-which-search-engine-En.md" );
+$html = $mde->md2html( "./samples/4-RAM-vs-HDD-En.md" );
 file_put_contents("./samples/test_" . $mde->getDocumentStyle() . $mde->getDocumentWidth() . ".html", $html);
-
 
 ?> 
