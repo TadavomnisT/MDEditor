a2d8c9aa109ce81231bb5827ff071729836ba7be
behrad.b <behroora@yahoo.com>
2024-05-29 13:12:57 +0330

    Add bash script for GUI

---
+++ start_gui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Start the PHP server in the MDEditor directory
+cd MDEditor
+php -S 127.0.0.1:8989 &
+
+# Open the URL in a browser
+sleep 2 # Wait for the server to start
+xdg-open http://127.0.0.1:8989/
